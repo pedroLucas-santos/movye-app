dc11ff7e259e249096849d81520fd2b9a484c87a
Pedro Lucas <110696140+pedroLucas-santos@users.noreply.github.com>
2024-11-17 17:53:56 -0300

    Revert "yoyoyo"
    This reverts commit 837b066765edadcef98ea0cb619c32a161c4066c.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    trailingSlash: true, // Required for GitHub Pages
-    images: {
-      unoptimized: true, // Required for static export
-    },
-    basePath: "/movye-app"
-};
+const nextConfig = {};
 
 export default nextConfig;
