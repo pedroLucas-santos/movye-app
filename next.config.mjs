837b066765edadcef98ea0cb619c32a161c4066c
Pedro Lucas <110696140+pedroLucas-santos@users.noreply.github.com>
2024-11-17 17:33:58 -0300

    yoyoyo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    trailingSlash: true, // Required for GitHub Pages
+    images: {
+      unoptimized: true, // Required for static export
+    },
+    basePath: "/movye-app"
+};
 
 export default nextConfig;
